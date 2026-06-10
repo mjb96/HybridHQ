d72cf5f14e21221411640071eab7d1178555b99e
mjb96 <153232585+mjb96@users.noreply.github.com>
2026-06-11 08:30:19 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // ==========================================
 // SERVICE WORKER (sw.js)
 // ==========================================
-const CACHE_NAME = 'hybrid-training-v34';
+const CACHE_NAME = 'hybrid-training-v35';
 
 const ASSETS_TO_CACHE = [
   './',
